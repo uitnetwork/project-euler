d00dde70f8abe0a094f899a1aa6296b32545e3e1
Ninh Doan <ninhdoan@Ninhs-MacBook-Pro.local>
2015-07-17 09:35:28 +0800

    First steps for project euler problems

---
+++ projecteuler-core/src/main/java/com/projecteuler/core/Main.java
@@ -0,0 +1,13 @@
+package com.projecteuler.core;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+   
+   public static void main(String[] args) {
+      List<String> strings=Arrays.asList("Hello world!","I'm Ninh");
+      strings.forEach(System.out::println);
+   }
+
+}
